0ff9b433162c02c9273957486dec29c437a380cb
webzone <webzone@gmail.com>
2018-02-12 14:45:47 -0800

    Update blake2b.c

--- stratum/algos/blake2b.c
+++ stratum/algos/blake2b.c
@@ -1,22 +1,22 @@
 /**
- * Blake2-S Implementation
- * tpruvot@github 2015-2016
+ * Blake2-B Implementation
+ * webzone@github 2018-2018
  */
 
 #include <string.h>
 #include <stdint.h>
 
-#include <sha3/blake2s.h>
+#include <sha3/blake2.h>
 
-void blake2s_hash(const char* input, char* output, uint32_t len)
+void blake2b_hash(const char* input, char* output, uint64_t len)
 {
-	uint8_t hash[BLAKE2S_OUTBYTES];
-	blake2s_state blake2_ctx;
+	uint16_t hash[BLAKE2B_OUTBYTES];
+	blake2b_state blake2_ctx;
 
-	blake2s_init(&blake2_ctx, BLAKE2S_OUTBYTES);
-	blake2s_update(&blake2_ctx, input, len);
-	blake2s_final(&blake2_ctx, hash, BLAKE2S_OUTBYTES);
+	blake2b_init(&blake2_ctx, BLAKE2B_OUTBYTES);
+	blake2b_update(&blake2_ctx, input, len);
+	blake2b_final(&blake2_ctx, hash, BLAKE2B_OUTBYTES);
 
-	memcpy(output, hash, 32);
+	memcpy(output, hash, 64);
 }
 
